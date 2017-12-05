6f01dec2873116cde95057789244dd13952db456
Gustav Blomér <gustav@blomer.se>
2017-12-05 20:33:57 +0100

    Day 4

---
+++ day4/day4part1.js
@@ -0,0 +1,44 @@
+const inputLoader = require('aoc-input-loader');
+const config = require('../config');
+
+inputLoader(2017, 4, config.aocSessionCookie).then(data => {
+  const noValid = data.split('\n').reduce((sum, passphrase) => {
+    const words = passphrase.split(/\s/);
+
+    for (let i = 0; i < words.length - 1; i++) {
+      for (var k = i + 1; k < words.length; k++) {
+        if (words[i] === words[k]) {
+          return sum;
+        }
+      }
+    }
+    return sum + 1;
+  }, 0);
+
+  console.log(noValid); // 383
+}, err => {
+  if (err) throw err;
+});
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+//
+// fs.readFile(path.join(__dirname, './example.txt'), 'utf8', function (err, data) {
+//   if (err) throw err;
+//   const noValid = data.split('\n').reduce((sum, passphrase) => {
+//     const words = passphrase.split(/\s/);
+//
+//     for (let i = 0; i < words.length - 1; i++) {
+//       for (var k = i + 1; k < words.length; k++) {
+//         if (words[i] === words[k]) {
+//           return sum;
+//         }
+//       }
+//     }
+//     return sum + 1;
+//   }, 0);
+//
+//   console.log(noValid);
+// });
