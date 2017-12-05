59ccbae55bd6a8e2bf350b473df39dd3458c4782
Gustav Blomér <gustav@blomer.se>
2017-12-05 20:07:09 +0100

    Day 5 and init npm/nvm

---
+++ config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+  aocSessionCookie: process.env.AOC_SESSION_COOKIE || '',
+};
---
+++ day5/day5part1.js
@@ -0,0 +1,34 @@
+const inputLoader = require('aoc-input-loader');
+const config = require('../config');
+
+inputLoader(2017, 5, config.aocSessionCookie).then(data => {
+  const jumps = data.split('\n').map(num => parseInt(num));
+
+  let steps = 0;
+  let i = 0;
+
+  while (i >= 0 && i < jumps.length) {
+    i += jumps[i]++;
+    steps++;
+  }
+  console.log(steps); // 391540
+}, err => {
+  if (err) throw err;
+});
+
+// const fs = require('fs');
+// const path = require('path');
+//
+// fs.readFile(path.join(__dirname, './example.txt'), 'utf8', function (err, data) {
+//   if (err) throw err;
+//   const jumps = data.split('\n').map(num => parseInt(num));
+//
+//   let steps = 0;
+//   let i = 0;
+//
+//   while (i >= 0 && i < jumps.length) {
+//     i += jumps[i]++;
+//     steps++;
+//   }
+//   console.log(steps);
+// });
