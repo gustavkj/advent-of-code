fac8595809d8b5acd7252361558d13f668222598
Gustav Utterheim <gustav@utterheim.com>
2020-12-01 12:56:02 +0100

    fix: make sure session token is set

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,14 @@
+import assert from 'assert';
 import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig();
 
+const aocSessionCookie = process.env.AOC_SESSION_COOKIE;
+
+assert(aocSessionCookie, 'AOC_SESSION_COOKIE needs to be set');
+
 const config = {
-  aocSessionCookie: process.env.AOC_SESSION_COOKIE || '',
+  aocSessionCookie: process.env.AOC_SESSION_COOKIE as string,
 };
 
 export default config;
