ec1a971e8ae164e65719b8563b3d4cf17b930b91
Gustav Utterheim <gustav@utterheim.com>
2020-12-06 18:04:34 +0100

    feat(2019): day 6

---
+++ src/2019/day6/index.spec.ts
@@ -0,0 +1,57 @@
+import { part1, part2 } from '.';
+
+describe('2020 day 6', () => {
+  describe('part 1', () => {
+    it('should count the direct and indirect orbits', () => {
+      const input = [
+        'COM)B',
+        'B)C',
+        'C)D',
+        'D)E',
+        'E)F',
+        'B)G',
+        'G)H',
+        'D)I',
+        'E)J',
+        'J)K',
+        'K)L',
+      ].join('\n');
+
+      expect(part1(input)).toEqual(42);
+    });
+
+    it('should count the direct and indirect orbits (COM - D)', () => {
+      const input = ['COM)B', 'B)C', 'C)D'].join('\n');
+
+      expect(part1(input)).toEqual(6);
+    });
+
+    it('should count the direct and indirect orbits (COM - D & H)', () => {
+      const input = ['COM)B', 'B)C', 'C)D', 'B)G', 'G)H'].join('\n');
+
+      expect(part1(input)).toEqual(11);
+    });
+  });
+
+  describe('part 2', () => {
+    it('should count the minimum number of orbital transfers required to move you to Santa', () => {
+      const input = [
+        'COM)B',
+        'B)C',
+        'C)D',
+        'D)E',
+        'E)F',
+        'B)G',
+        'G)H',
+        'D)I',
+        'E)J',
+        'J)K',
+        'K)L',
+        'K)YOU',
+        'I)SAN',
+      ].join('\n');
+
+      expect(part2(input)).toEqual(4);
+    });
+  });
+});
---
+++ src/2019/day6/index.ts
@@ -0,0 +1,79 @@
+interface SpaceObject {
+  name: string;
+  orbitedBy?: string[];
+  orbits?: string;
+}
+
+function parseInput(input: string) {
+  const orbits: Record<string, SpaceObject> = {};
+
+  input.split('\n').forEach((orbit) => {
+    const [center, satellite] = orbit.split(')');
+
+    orbits[center] = {
+      ...(orbits[center] ?? {}),
+      name: center,
+      orbitedBy: [...((orbits[center] ?? {}).orbitedBy ?? []), satellite],
+    };
+
+    orbits[satellite] = {
+      ...(orbits[satellite] ?? {}),
+      name: satellite,
+      orbits: center,
+    };
+  });
+
+  return orbits;
+}
+
+function getOrbitCount(
+  name: string,
+  orbits: Record<string, SpaceObject>,
+  indirectOrbitsOfParent = 0,
+): number {
+  const satellite = orbits[name];
+  const indirectOrbits = indirectOrbitsOfParent + (satellite.orbits ? 1 : 0);
+
+  if (satellite.orbitedBy) {
+    return satellite.orbitedBy.reduce(
+      (sum, childSatellite) => sum + getOrbitCount(childSatellite, orbits, indirectOrbits),
+      indirectOrbits,
+    );
+  }
+
+  return indirectOrbits;
+}
+
+export function part1(input: string): number {
+  const orbits = parseInput(input);
+
+  return getOrbitCount('COM', orbits); // 358244
+}
+
+function getOrbitChain(name: string, orbits: Record<string, SpaceObject>): string[] {
+  const satellite = orbits[name];
+
+  if (satellite.orbits) {
+    return [...getOrbitChain(satellite.orbits, orbits), satellite.orbits];
+  }
+
+  return [];
+}
+
+export function part2(input: string): number {
+  const orbits = parseInput(input);
+
+  const yourOrbitChain = getOrbitChain('YOU', orbits);
+  const santasOrbitChain = getOrbitChain('SAN', orbits);
+
+  let split = 0;
+
+  while (yourOrbitChain[split] === santasOrbitChain[split]) {
+    split += 1;
+  }
+
+  // No need to backtrack one step since the last object in each chains are the ones orbited currently,
+  // so they are not included in the transfer count.
+
+  return yourOrbitChain.length + santasOrbitChain.length - 2 * split; // 517
+}
