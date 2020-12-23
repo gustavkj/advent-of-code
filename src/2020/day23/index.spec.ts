40b31e60d611d04758d4b41a5af303a0ee9c827e
Gustav Utterheim <gustav@utterheim.com>
2020-12-23 20:39:09 +0100

    feat(2020): day 23

---
+++ src/2020/day23/index.spec.ts
@@ -0,0 +1,15 @@
+import { part1, part2 } from '.';
+
+describe('2020 day 23', () => {
+  describe('part 1', () => {
+    it('should return the labels on the cups after cup 1', () => {
+      expect(part1('389125467')).toEqual('67384529');
+    });
+  });
+
+  describe('part 2', () => {
+    it('should return the product of the two cups clockwise of cup 1', () => {
+      expect(part2('389125467')).toEqual(149245887792);
+    });
+  });
+});
---
+++ src/2020/day23/index.ts
@@ -0,0 +1,98 @@
+export function part1(input: string): string {
+  const cups = input.split('').map(Number);
+
+  const moves = 100;
+  let i = 0;
+  const maxCup = Math.max(...cups);
+
+  while (i < moves) {
+    const currentCup = cups[0];
+    const cupsToMove = cups.splice(1, 3);
+
+    const preliminaryDestinations = cups.filter((cup) => !cupsToMove.includes(cup));
+    let destinationCup = currentCup - 1;
+    while (!preliminaryDestinations.includes(destinationCup)) {
+      if (destinationCup === 0) {
+        destinationCup = maxCup;
+      } else {
+        destinationCup -= 1;
+      }
+    }
+
+    const destinationCupIndex = cups.findIndex((cup) => cup === destinationCup);
+
+    cups.splice(destinationCupIndex + 1, 0, ...cupsToMove);
+
+    cups.push(cups.shift() as number);
+    i += 1;
+  }
+
+  const [end, start] = cups.join('').split('1');
+
+  return `${start}${end}`;
+}
+
+interface Cup {
+  label: number;
+  next: number;
+}
+
+export function part2(input: string): number {
+  const startingCups = input.split('').map(Number);
+  const totalNumberOfCups = 1000000;
+  const cups = new Map(
+    startingCups.map((label, index, array) => [
+      label,
+      { label, next: array[index + 1] ?? 10 } as Cup,
+    ]),
+  );
+
+  for (let k = 10; k <= totalNumberOfCups; k += 1) {
+    cups.set(k, { label: k, next: k + 1 > totalNumberOfCups ? startingCups[0] : k + 1 });
+  }
+
+  const moves = 10000000;
+  let i = 0;
+  const maxCup = totalNumberOfCups;
+
+  let currentCup = cups.get(startingCups[0]) as Cup;
+
+  while (i < moves) {
+    const cupsToMove = [];
+    let nextCup = cups.get(currentCup.next) as Cup;
+
+    while (cupsToMove.length < 3) {
+      cupsToMove.push(nextCup.label);
+      nextCup = cups.get(nextCup.next) as Cup;
+    }
+
+    let destinationCupLabel = currentCup.label - 1;
+    while (cupsToMove.includes(destinationCupLabel) || destinationCupLabel === 0) {
+      if (destinationCupLabel === 0) {
+        destinationCupLabel = maxCup;
+      } else {
+        destinationCupLabel -= 1;
+      }
+    }
+
+    const destinationCup = cups.get(destinationCupLabel) as Cup;
+
+    const lastCup = cups.get(cupsToMove[2]) as Cup;
+    currentCup.next = lastCup.next;
+    lastCup.next = destinationCup.next;
+    [destinationCup.next] = cupsToMove;
+
+    cups.set(currentCup.label, currentCup);
+    cups.set(destinationCup.label, destinationCup);
+    cups.set(lastCup.label, lastCup);
+
+    currentCup = cups.get(currentCup.next) as Cup;
+    i += 1;
+  }
+
+  const cupOne = cups.get(1) as Cup;
+  const cupTwo = cups.get(cupOne.next) as Cup;
+  const cupThree = cups.get(cupTwo.next) as Cup;
+
+  return cupTwo.label * cupThree.label;
+}
