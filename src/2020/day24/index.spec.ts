4291d4d39f39d98f1cde8266fa0fa2eeb773e80c
Gustav Utterheim <gustav@utterheim.com>
2020-12-24 15:49:58 +0100

    feat(2020): day 24

---
+++ src/2020/day24/index.spec.ts
@@ -0,0 +1,38 @@
+import { part1, part2 } from '.';
+
+describe('2020 day 24', () => {
+  const input = [
+    'sesenwnenenewseeswwswswwnenewsewsw',
+    'neeenesenwnwwswnenewnwwsewnenwseswesw',
+    'seswneswswsenwwnwse',
+    'nwnwneseeswswnenewneswwnewseswneseene',
+    'swweswneswnenwsewnwneneseenw',
+    'eesenwseswswnenwswnwnwsewwnwsene',
+    'sewnenenenesenwsewnenwwwse',
+    'wenwwweseeeweswwwnwwe',
+    'wsweesenenewnwwnwsenewsenwwsesesenwne',
+    'neeswseenwwswnwswswnw',
+    'nenwswwsewswnenenewsenwsenwnesesenew',
+    'enewnwewneswsewnwswenweswnenwsenwsw',
+    'sweneswneswneneenwnewenewwneswswnese',
+    'swwesenesewenwneswnwwneseswwne',
+    'enesenwswwswneneswsenwnewswseenwsese',
+    'wnwnesenesenenwwnenwsewesewsesesew',
+    'nenewswnwewswnenesenwnesewesw',
+    'eneswnwswnwsenenwnwnwwseeswneewsenese',
+    'neswnwewnwnwseenwseesewsenwsweewe',
+    'wseweeenwnesenwwwswnew',
+  ].join('\n');
+
+  describe('part 1', () => {
+    it('should return how many tiles are left with the black side up', () => {
+      expect(part1(input)).toEqual(10);
+    });
+  });
+
+  describe('part 2', () => {
+    it('should return how many tiles will be black after 100 days', () => {
+      expect(part2(input)).toEqual(2208);
+    });
+  });
+});
---
+++ src/2020/day24/index.ts
@@ -0,0 +1,125 @@
+import { Point } from '../../types';
+
+const deltas: Record<string, Point> = {
+  ne: { x: 0.5, y: 0.5 },
+  e: { x: 1, y: 0 },
+  se: { x: 0.5, y: -0.5 },
+  sw: { x: -0.5, y: -0.5 },
+  w: { x: -1, y: 0 },
+  nw: { x: -0.5, y: 0.5 },
+};
+
+function getDelta(direction: string): Point {
+  const delta = deltas[direction];
+
+  if (!delta) {
+    throw new Error(`Unknown direction: ${direction}`);
+  }
+
+  return delta;
+}
+
+function pointToString({ x, y }: Point) {
+  return `${x},${y}`;
+}
+
+function getStartingState(input: string) {
+  const instructions = input.split('\n').map((line) => {
+    const reg = /(se|ne|sw|nw|e|w)/g;
+    let result: RegExpExecArray | null;
+    const dir = [];
+
+    // eslint-disable-next-line no-cond-assign
+    while ((result = reg.exec(line)) !== null) {
+      dir.push(result[1]);
+    }
+
+    return dir;
+  });
+
+  const flippedTiles = new Map<string, boolean>();
+  const referencePoint: Point = { x: 0, y: 0 };
+
+  instructions
+    .map((instruction) => {
+      const tile = { ...referencePoint };
+      instruction.forEach((step) => {
+        const { x: dx, y: dy } = getDelta(step);
+        tile.x += dx;
+        tile.y += dy;
+      });
+      return tile;
+    })
+    .forEach((tile) => {
+      const key = pointToString(tile);
+      const currentState = flippedTiles.get(key) ?? false;
+      flippedTiles.set(key, !currentState);
+    });
+
+  return { flippedTiles };
+}
+
+function getNumberOfFlippedTiles(flippedTiles: Map<string, boolean>) {
+  return [...flippedTiles.values()].reduce((sum, isFlipped) => sum + (isFlipped ? 1 : 0), 0);
+}
+
+export function part1(input: string): number {
+  const { flippedTiles } = getStartingState(input);
+
+  return getNumberOfFlippedTiles(flippedTiles); // 373
+}
+
+function getNeighborsCoordinates(tile: Point) {
+  return Object.values(deltas).map(
+    ({ x: dx, y: dy }): Point => ({
+      x: tile.x + dx,
+      y: tile.y + dy,
+    }),
+  );
+}
+
+function getActiveNeighbors(tile: Point, flippedTiles: Map<string, boolean>) {
+  return getNeighborsCoordinates(tile)
+    .map((neighborTile) => flippedTiles.get(pointToString(neighborTile)) ?? false)
+    .filter(Boolean).length;
+}
+
+function stringToPoint(value: string): Point {
+  const [x, y] = value.split(',').map(Number);
+
+  return { x, y };
+}
+
+export function part2(input: string): number {
+  const { flippedTiles } = getStartingState(input);
+
+  let day = 1;
+  while (day <= 100) {
+    const coordsToGoThrough = new Set(
+      [...flippedTiles.keys()]
+        .map(stringToPoint)
+        .flatMap((tile) => [tile, ...getNeighborsCoordinates(tile)]),
+    );
+    const currentState = new Map(flippedTiles);
+
+    coordsToGoThrough.forEach((tile) => {
+      const key = pointToString(tile);
+      const isFlipped = currentState.get(key) ?? false;
+
+      const activeNeighbors = getActiveNeighbors(tile, currentState);
+
+      if (isFlipped && activeNeighbors > 2) {
+        flippedTiles.set(key, false);
+      } else if (!isFlipped && activeNeighbors === 2) {
+        flippedTiles.set(key, true);
+      } else if (activeNeighbors === 0) {
+        flippedTiles.delete(key);
+      }
+    });
+
+    day += 1;
+  }
+
+  // 3917
+  return getNumberOfFlippedTiles(flippedTiles);
+}
