761a5e5318c026d6c8fdd347537fe5688e74abfa
Gustav Utterheim <gustav@utterheim.com>
2021-12-13 20:47:28 +0100

    test(2021): correct test name for day 7

--- src/2021/day07/index.spec.ts
+++ src/2021/day07/index.spec.ts
@@ -2,7 +2,7 @@ import { part1, part2 } from '.';
 
 const input = '16,1,2,0,4,2,7,1,2,14';
 
-describe('2021 day 6', () => {
+describe('2021 day 7', () => {
   describe('part 1', () => {
     it('should return how much fuel the crabs must spend to align to the position requiring minimum fuel', () => {
       expect(part1(input)).toBe(37);
