9b185629bb38b27f255150499bce471a8b53b87c
Gustav Utterheim <gustav@utterheim.com>
2023-10-29 14:12:17 +0100

    fix: use built-in .env handling in node 20

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,11 @@
 import assert from 'assert';
-import { config as dotenvConfig } from 'dotenv';
-
-dotenvConfig();
 
 const aocSessionCookie = process.env.AOC_SESSION_COOKIE;
 
 assert(aocSessionCookie, 'AOC_SESSION_COOKIE needs to be set');
 
 const config = {
-  aocSessionCookie: process.env.AOC_SESSION_COOKIE!,
+  aocSessionCookie,
 };
 
 export default config;
